054b9f1d6178e649a1778bd6479132faa6e95142
dolsotbob <1hyoon3@naver.com>
2025-05-13 17:39:07 +0900

    modified

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { findUserByUserId, verifyUser } = require('../services/auth.service');
 const { secretKey } = require('../config');
+// const jsonwebtoken = require('jsonwebtoken');
 
 module.exports = {
   login: async (req, res) => {
@@ -12,9 +13,30 @@ module.exports = {
             토큰을 생성하여
             200 응답코드와 함께 token의 값으로 토큰을 반환
     */
-    return res.json('not implemented');
+    // 필수 입력 체크 
+    if (!userId || !password) {
+      return res.status(401).json({ message: '등록되지 않은 유저입니다.' });
+    }
+
+    // 유저 확인 
+    // verifyUser 함수는 src/services/auth.service.js에 있음 
+    const isValidUser = await verifyUser(userId, password);
+    if (!isValidUser) {
+      return res.status(401).json({ message: '등록되지 않은 유저입니다.' });
+    }
+
+    // 토큰 생성 
+    // sign 눌러서 뭐를 인자로 쓸지 확인하면 됨 
+    const token = jwt.sign({ userId }, secretKey, {
+      expiresIn: '15m',
+      issuer: 'jwt-issuer',
+    });
+    // console.log(token);
+
+    return res.status(200).json({ token });
   },
 
+  // 토큰 없으면 401, 토큰 있으면 userId 기반 유저 정보 반환
   me: async (req, res) => {
     const { userId } = req.decoded;
     /*
@@ -22,6 +44,11 @@ module.exports = {
         200 응답코드와 함께 user의 값으로 유저 정보를 반환합니다.
         - 반환해야 하는 데이터 양식은 테스트 코드를 통해 확인합니다.
     */
-    return res.json('not implemented');
+    const user = await findUserByUserId(userId);
+    if (!user) {
+      return res.status(404).json({ message: '유저를 찾을 수 없습니다.' });
+    }
+
+    return res.status(200).json({ user });
   },
 };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,21 +1,46 @@
 const jwt = require('jsonwebtoken');
 const { secretKey } = require('../config');
+const { TokenExpiredError } = require('jsonwebtoken');
+const { JsonWebTokenError } = require('jsonwebtoken');
 
 module.exports = {
   auth: (req, res, next) => {
-    try {
-      /*
-        Todo: 요청 헤더에서 JWT를 추출하고, 토큰을 검증한 후
-            verify하여 디코딩된 정보를 req.decoded에 저장합니다.
-            이후 다음 프로세스를 위해 next()을 실행합니다.
-      */
-      return res.json('not implemented');
-    } catch (err) {
-      /*
+    /*
+       Todo: 요청 헤더에서 JWT를 추출하고, 토큰을 검증한 후
+           verify하여 디코딩된 정보를 req.decoded에 저장합니다.
+           이후 다음 프로세스를 위해 next()을 실행합니다.
+     */
+    /*
         Todo: err.name에 따라 조건에 맞는 응답을 반환합니다. 
           - TokenExpiredError : 419 응답코드와 함께 "토큰이 만료되었습니다."를 반환
           - JsonWebTokenError : 401 응답코드와 함께 "유효하지 않은 토큰입니다."를 반환
       */
+    const authHeader = req.headers.authorization;
+
+    // 토큰 없음 
+    if (!authHeader) {
+      return res.status(401).json({ message: '토큰이 없습니다.' });
+    }
+
+    const token = authHeader.split(' ')[0] === 'Bearer'
+      ? authHeader.split(' ')[1]
+      : authHeader;
+    // console.log(token);
+
+    try {
+      // console.log(req.headers.authorization);
+      const decoded = jwt.verify(token, secretKey);
+      req.decoded = decoded;
+      next();
+    } catch (err) {
+      // console.log(err.name);
+      if (err.name === 'TokenExpiredError') {
+        return res.status(419).json({ message: "토큰이 만료되었습니다." });
+      } else if (err.name === 'JsonWebTokenError') {
+        return res.status(401).json({ message: "유효하지 않은 토큰입니다." });
+      } else {
+        return res.status(500).json({ message: "서버 오류입니다." })
+      }
     }
   },
 };
